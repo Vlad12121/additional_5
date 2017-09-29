328ecb83150ac43f1f70c49accc6ced9918ccc2c
VladRomanuk <MR.3579STELS@gmail.com>
2017-09-29 20:18:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  	let string=str.split('');
+  	return findClose(string,bracketsConfig);
+}
+
+function findClose(ar,config){
+	let stack=[];
+	for(let i=0;i<ar.length;i++){
+		let rule=findRule(ar[i],config);
+		if(rule[0]===rule[1]){
+			if((stack.length===0)||(stack[stack.length-1]!==rule[0])) stack.push(ar[i]);
+			else stack.pop();
+		}
+		else {
+			if(ar[i]===rule[0]) stack.push(ar[i]);
+			else if((ar[i]===rule[1])&&(stack[stack.length-1]===rule[0])) stack.pop();
+			else if((ar[i]===rule[1])&&(stack[stack.length-1]!==rule[0])) stack.push(ar[i]);
+		}
+	}
+	return stack.length===0?true:false;
+}
+
+function findRule(el,config){
+	for(let rule of config){
+		if((el===rule[0])||(el===rule[1]))
+			return rule;
+	}
+	return false;
 }
